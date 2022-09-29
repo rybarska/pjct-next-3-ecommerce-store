6f63b1d12f534c1b29feb4309989e757a8c0f1ca
agata <rybarska>
2022-09-29 23:27:29 +0200

    Exchange favicon and one photo

--- database/mirages.js
+++ database/mirages.js
@@ -7,46 +7,46 @@ export const mirages = [
   {
     id: 1,
     name: 'Aurora borealis',
-    price: '1000 Mirage€',
+    price: '999999 μ€',
     description:
       "a natural light display in Earth's sky; result of disturbances in the magnetosphere caused by the solar wind"
   },
   {
     id: 2,
     name: 'Brocken spectre',
-    price: '1000 Mirage€',
+    price: '999999 μ€',
     description:
       "the magnified shadow of an observer cast in mid air upon any type of cloud opposite to a strong light source"
   },
   {
     id: 3,
     name: 'Fata morgana',
-    price: '1000 Mirage€',
+    price: '999999 μ€',
     description: "superior mirage visible in a narrow band right above the horizon; occurs because rays of light bend when they pass through air layers of different temperatures in a steep thermal inversion where an atmospheric duct has formed"
   },
   {
     id: 4,
     name: 'Green flash',
-    price: '1000 Mirage€',
+    price: '999999 μ€',
     description: "a distinct green spot is briefly visible above the upper rim of the Sun's disk; occur because the Earth's atmosphere can cause the light from the Sun to refract into different colors",
   },
   {
     id: 5,
     name: 'Light pillars',
-    price: '1000 Mirage€',
+    price: '999999 μ€',
     description: "vertical beams of light that appear to extend above and/or below a light source; created by the reflection of light from tiny ice crystals that are suspended in the atmosphere or that comprise high-altitude clouds",
   },
   {
     id: 6,
     name: 'Moon rainbow',
-    price: '1000 Mirage€',
+    price: '999999 μ€',
     description: "a rainbow produced by moonlight, caused by the refraction of light in many water droplets; much fainter than solar rainbows and very rarely seen",
   },
   {
     id: 7,
     name: 'Omega sun',
-    price: '1000 Mirage€',
+    price: '999999 μ€',
     description: "the sun appearing in shape of the greek letter omega; the lower part is an inverted image produced by refraction by a layer of warmer and less dense air close to the ocean surface",
   },
-  { id: 8, name: 'Sundog', price: '1000 Mirage€', description: "colored spots of light that develop due to the refraction of light through ice crystals" },
+  { id: 8, name: 'Sundog', price: '999999 μ€', description: "colored spots of light that develop due to the refraction of light through ice crystals" },
 ];
